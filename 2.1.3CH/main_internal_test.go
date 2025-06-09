bc6f30d89a67242498f2b59763cc2f101d7cbbab
steven <s.ntentopoulos@gmail.com>
2025-06-09 23:28:04 +0300

    2.1.3CH

---
+++ 2.1.3CH/main_internal_test.go
@@ -0,0 +1,20 @@
+package main
+
+func ExampleMain() {
+	main() // Calls the main function
+
+	// Output:
+	// Hello world
+}
+
+/*With the
+// Output:
+// Hello world
+we specify the result of the main function call.
+This is useful for testing the output of the main function.
+want:
+Hello world
+FAIL
+exit status 1
+FAIL    learngo-pockets/hello   0.022s
+*/
